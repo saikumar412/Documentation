971de007693d145e21d08763fffcdd51c4c5f6f6
saikumar412 <ekula.saikumar@gmail.com>
2018-08-24 15:50:42 +0530

    reset jenkins user details

--- jenkins.sh
+++ jenkins.sh
@@ -9,3 +9,19 @@
  sudo service jenkins start
  sudo service jenkins status
  sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+ 
+ 
+ 
+ Solution – Need to execute the below steps as root, after every Jenkins backup restore.
+
+	sudo mkdir /var/lib/jenkins/.kube/
+	chmod 777 /var/lib/jenkins/.kube/
+	sudo cp /root/.kube/config /var/lib/jenkins/.kube/
+	sudo chmod 444 /var/lib/jenkins/.kube/config
+
+and verify what is the access given to Jenkins in visudo. (right now it is in Root access)
+
+	visudo
+jenkins    ALL=(ALL)    NOPASSWD: ALL
+
+going on we will create a separate group, in which we will add Jenkins and that group will have permissions to execute Kubectl, Docker and helm.
